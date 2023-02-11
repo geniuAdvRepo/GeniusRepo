a7769961fefaf25b7b995755d278934c0740c51b
Nathan Soares Silva <nathan.silva@teros.com.br>
2023-02-11 16:22:04 -0300

    initial commit

---
+++ src/Components/misc/navbar/navBar.tsx
@@ -0,0 +1,99 @@
+import React, { useState, CSSProperties, useEffect } from "react";
+import { motion, Variants } from "framer-motion";
+import Image from "next/image";
+import "./styles.module.css";
+
+const itemVariants: Variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+};
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="fixed z-50 w-full ">
+      <motion.nav initial={false} animate={isOpen ? "open" : "closed"} className="menu border-b  flex md:items-center px-12 bg-white justify-between pb-0 p-4 shadow-lg md:pb-4 w-full">
+        <div className="flex items-center justify-between mb-4  md:mb-0">
+          <a className="flex justify-start w-2/3 h-16 no-underline text-grey-darkest  hover:text-black" href="#">
+            <img src="./images/logo sf dg.png" alt="" className=" h-full flex object-contain" />
+          </a>
+
+          <a className="text-black hover:text-orange md:hidden font-bold" href="#">
+            <i className="fa fa-2x fa-bars"></i>
+          </a>
+        </div>
+        <nav className="hidden lg:flex  md:grid">
+          <ul className="list-reset md:flex md:items-center">
+            <li className="md:ml-4">
+              <a className="block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" href="#">
+                Products
+              </a>
+            </li>
+            <li className="md:ml-4">
+              <a className="border-t block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" href="#">
+                About
+              </a>
+            </li>
+            <li className="md:ml-4">
+              <a className="border-t block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex md:hidden lg:hidden">
+          <motion.button whileTap={{ scale: 0.97 }} onClick={() => setIsOpen(!isOpen)} id="button">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+              <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+          </motion.button>
+        </div>
+      </motion.nav>
+      <motion.nav initial={false} animate={isOpen ? "open" : "closed"} className="menu  w-full m-0">
+        <motion.ul
+          id="ul"
+          variants={{
+            open: {
+              clipPath: "inset(0% 0% 0% 0% round 0px)",
+              opacity: 1,
+              transition: {
+                type: "spring",
+                bounce: 0,
+                duration: 0.7,
+                delayChildren: 0.3,
+                staggerChildren: 0.05,
+              },
+            },
+            closed: {
+              clipPath: "inset(0% 0% 90% 0% round 0px)",
+              opacity: 0,
+              transition: {
+                type: "spring",
+                bounce: 0,
+                duration: 0.5,
+              },
+            },
+          }}
+          style={{ pointerEvents: isOpen ? "auto" : "none" }}
+        >
+          <motion.li id="li" variants={itemVariants}>
+            Item 1{" "}
+          </motion.li>
+          <motion.li id="li" variants={itemVariants}>
+            Item 2{" "}
+          </motion.li>
+          <motion.li id="li" variants={itemVariants}>
+            Item 3{" "}
+          </motion.li>
+        </motion.ul>
+      </motion.nav>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/Components/pages/initial/entry.tsx
@@ -0,0 +1,64 @@
+import { Services } from "./services";
+
+export const Entry = () => {
+  return (
+    <div className=" pt-20">
+      <div
+        className="bg-gradient-to-r 
+                from-blue-900 
+                to-slate-800 
+                via-indigo-900
+                animate-gradient-x"
+      >
+        <div className="px-4 py-10 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto">
+              <span className="relative inline-block">
+                <svg viewBox="0 0 52 24" fill="currentColor" className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-gray-400 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                  <defs>
+                    <pattern id="dc223fcc-6d72-4ebc-b4ef-abe121034d6e" x="0" y="0" width=".135" height=".30">
+                      <circle cx="1" cy="1" r=".7" />
+                    </pattern>
+                  </defs>
+                  <rect fill="url(#dc223fcc-6d72-4ebc-b4ef-abe121034d6e)" width="52" height="24" />
+                </svg>
+                <span className="relative">The</span>
+              </span>{" "}
+              quick, brown fox jumps over a lazy dog
+            </h2>
+            <p className="text-base text-gray-300 md:text-lg">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.</p>
+          </div>
+          <div className="flex items-center sm:justify-center">
+            <a href="#_" className="relative px-5 py-2 font-medium text-white group">
+              <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-amber-700 group-hover:bg-amber-700 group-hover:skew-x-12"></span>
+              <span
+                className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-gradient-to-r 
+from-amber-500
+to-amber-600
+ group-hover:bg-amber-700 group-hover:-skew-x-12"
+              ></span>
+              <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
+              <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
+              <span className="relative flex gap-2 fill-white hover:fill-green-300 ">
+                Agendar Agora{" "}
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" className="">
+                  <path d="M 12.011719 2 C 6.5057187 2 2.0234844 6.478375 2.0214844 11.984375 C 2.0204844 13.744375 2.4814687 15.462563 3.3554688 16.976562 L 2 22 L 7.2324219 20.763672 C 8.6914219 21.559672 10.333859 21.977516 12.005859 21.978516 L 12.009766 21.978516 C 17.514766 21.978516 21.995047 17.499141 21.998047 11.994141 C 22.000047 9.3251406 20.962172 6.8157344 19.076172 4.9277344 C 17.190172 3.0407344 14.683719 2.001 12.011719 2 z M 12.009766 4 C 14.145766 4.001 16.153109 4.8337969 17.662109 6.3417969 C 19.171109 7.8517969 20.000047 9.8581875 19.998047 11.992188 C 19.996047 16.396187 16.413812 19.978516 12.007812 19.978516 C 10.674812 19.977516 9.3544062 19.642812 8.1914062 19.007812 L 7.5175781 18.640625 L 6.7734375 18.816406 L 4.8046875 19.28125 L 5.2851562 17.496094 L 5.5019531 16.695312 L 5.0878906 15.976562 C 4.3898906 14.768562 4.0204844 13.387375 4.0214844 11.984375 C 4.0234844 7.582375 7.6067656 4 12.009766 4 z M 8.4765625 7.375 C 8.3095625 7.375 8.0395469 7.4375 7.8105469 7.6875 C 7.5815469 7.9365 6.9355469 8.5395781 6.9355469 9.7675781 C 6.9355469 10.995578 7.8300781 12.182609 7.9550781 12.349609 C 8.0790781 12.515609 9.68175 15.115234 12.21875 16.115234 C 14.32675 16.946234 14.754891 16.782234 15.212891 16.740234 C 15.670891 16.699234 16.690438 16.137687 16.898438 15.554688 C 17.106437 14.971687 17.106922 14.470187 17.044922 14.367188 C 16.982922 14.263188 16.816406 14.201172 16.566406 14.076172 C 16.317406 13.951172 15.090328 13.348625 14.861328 13.265625 C 14.632328 13.182625 14.464828 13.140625 14.298828 13.390625 C 14.132828 13.640625 13.655766 14.201187 13.509766 14.367188 C 13.363766 14.534188 13.21875 14.556641 12.96875 14.431641 C 12.71875 14.305641 11.914938 14.041406 10.960938 13.191406 C 10.218937 12.530406 9.7182656 11.714844 9.5722656 11.464844 C 9.4272656 11.215844 9.5585938 11.079078 9.6835938 10.955078 C 9.7955938 10.843078 9.9316406 10.663578 10.056641 10.517578 C 10.180641 10.371578 10.223641 10.267562 10.306641 10.101562 C 10.389641 9.9355625 10.347156 9.7890625 10.285156 9.6640625 C 10.223156 9.5390625 9.737625 8.3065 9.515625 7.8125 C 9.328625 7.3975 9.131125 7.3878594 8.953125 7.3808594 C 8.808125 7.3748594 8.6425625 7.375 8.4765625 7.375 z"></path>
+                </svg>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="relative px-4 sm:px-0">
+        <div
+          className="absolute inset-0  bg-gradient-to-r 
+from-blue-900 
+to-slate-800 
+via-indigo-900
+animate-gradient-x h-1/2"
+        />
+        <Services />
+      </div>
+    </div>
+  );
+};
